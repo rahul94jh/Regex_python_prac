b767d3556888e9ca35c69c6de3c7ca8dfc4e9dcb
Rahul <rahul1994jh@gmail.com>
2020-05-26 12:07:34 +0530

    initial commit

---
+++ regex_char_sets.py
@@ -0,0 +1,18 @@
+import re
+
+
+def find_pattern(text, patterns):
+    if re.search(patterns, text):
+        return re.search(patterns, text)
+    else:
+        return "Not Found!"
+
+
+# [' and ']'.
+# They're used for specifying a character class, which is a set of characters that you wish to match.
+# Characters can be listed individually as follows
+
+print(find_pattern("a", "[abc]"))  # match
+# Or a range of characters can be indicated by giving two characters and separating them by a '-'.
+print(find_pattern("b", "[a-c]"))  # match
+print(find_pattern("d", "[a-c]"))  # no match
